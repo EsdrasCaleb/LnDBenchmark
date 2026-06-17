c51c11e58906145f9246f25b49c206f3111838c4
Esdras Caleb Oliveira Silva <esdrascaleb@gmail.com>
2026-06-17 09:10:22 -0300

    debugs

--- in_container/orchestrator.py
+++ in_container/orchestrator.py
@@ -283,7 +283,7 @@ def get_best_code_models(limit=5, completed_models=None):
     api = HfApi(token=hf_token)
 
     # Foca explicitamente em geração de texto
-    available_models = api.list_models(filter=["code", "text-generation"], sort="trending_score", full=True)
+    available_models = api.list_models(filter=["code", "text-generation"], sort=["trending_score"],direction=-1, full=True)
 
     filtered_models = []
     for model in available_models:
