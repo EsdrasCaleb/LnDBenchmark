383da9673df1a079a2bfdfc6a67ae356509a8536
Esdras Caleb Oliveira Silva <esdrascaleb@gmail.com>
2026-06-20 14:37:15 -0300

    tring a new approch

--- in_container/orchestrator.py
+++ in_container/orchestrator.py
@@ -116,6 +116,9 @@ def run_unity_pipeline(model_safe_name, model_dir, backend_name):
     subprocess.run([
         "/opt/Unity/Unity", "-projectPath", project_path, "-batchmode", "-nographics",
         "-executeMethod", "LaundryNDishes.CLI.LndCommandLineInterface.ExportTestReport",
+        "-no-licensing"
+       "-disable-assembly-updater"
+        "-aad"
         "-csv", test_list_path,
         "-logFile", f"{model_dir}/export-cli-log.txt"
     ], check=False)
@@ -144,6 +147,9 @@ def run_unity_pipeline(model_safe_name, model_dir, backend_name):
         "-testPlatform", "editmode", "-runTests", "-debugCodeOptimization", "-enableCodeCoverage",
         "-coverageResultsPath", coverage_dir,
         "-coverageOptions", f"generateAdditionalMetrics;assemblyFilters:+{asm_name}",
+        "-disable-assembly-updater"
+        "-aad"
+        "-no-licensing"
         "-testResults", f"{model_dir}/editmode-results.xml",
         "-logFile", f"{model_dir}/editmode-log.txt"
     ], check=False)
@@ -153,6 +159,9 @@ def run_unity_pipeline(model_safe_name, model_dir, backend_name):
         "/opt/Unity/Unity", "-projectPath", project_path, "-batchmode", "-nographics",
         "-testPlatform", "playmode", "-runTests", "-debugCodeOptimization", "-enableCodeCoverage",
         "-coverageResultsPath", coverage_dir,
+        "-disable-assembly-updater"
+        "-aad"
+        "-no-licensing"
         "-coverageOptions", f"generateAdditionalMetrics;assemblyFilters:+{asm_name}",
         "-testResults", f"{model_dir}/playmode-results.xml",
         "-logFile", f"{model_dir}/playmode-log.txt"
@@ -162,6 +171,9 @@ def run_unity_pipeline(model_safe_name, model_dir, backend_name):
     subprocess.run([
         "/opt/Unity/Unity", "-projectPath", project_path, "-batchmode", "-nographics",
         "-debugCodeOptimization", "-enableCodeCoverage", "-coverageResultsPath", coverage_dir,
+        "-disable-assembly-updater"
+        "-aad"
+        "-no-licensing"
         "-coverageOptions", f"generateHtmlReport;generateBadgeReport;assemblyFilters:+{asm_name}",
         "-quit", "-logFile", f"{model_dir}/coverage-report-log.txt"
     ], check=False)
@@ -170,6 +182,9 @@ def run_unity_pipeline(model_safe_name, model_dir, backend_name):
     subprocess.run([
         "/opt/Unity/Unity", "-projectPath", project_path, "-batchmode", "-nographics",
         "-executeMethod", "LaundryNDishes.CLI.LndCommandLineInterface.ExportTestReport",
+        "-disable-assembly-updater"
+        "-aad"
+        "-no-licensing"
         "-csv", f"{model_dir}/testList.csv",
         "-logFile", f"{model_dir}/export-cli-log.txt"
     ], check=False)
